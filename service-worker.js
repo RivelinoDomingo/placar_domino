b877256f46774f3784bf739b07831e39bc9df443
Rivelino Domingo de Oliveira <rivelin95@gmail.com>
2026-03-02 02:01:31 -0300

    atualização de cache para forçar novas mudanças!

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 // 2. Mantém sua lógica de Cache (PWA)
-const CACHE_NAME = "placar_domino_cache_v1.4";
+const CACHE_NAME = "placar_domino_cache_v1.5";
 const urlsToCache = [
   "/placar_domino/",
   "/placar_domino/index.html",
